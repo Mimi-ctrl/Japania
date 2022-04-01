a0317e7d082da73b0f8399cb0e720d66fc3ec8b0
Laura Peltonen <laura.peltonen53@gmail.com>
2022-04-01 20:32:27 +0300

    templates

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("front_page.html")
+
+@app.route("/hiraganat_ja_katakanat")
+def page1():
+    return render_template("hiraganakatakana.html")
+
+@app.route("/sanastoa")
+def page2():
+    return render_template("words.html")
+
