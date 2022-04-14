fe2ed5dada2fee79278b8471e7d275ef98d3777a
Laura Peltonen <laura.peltonen53@gmail.com>
2022-04-14 12:58:58 +0300

    routes moved to their own file

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
+from os import getenv
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    return render_template("front_page.html")
-
-@app.route("/hiraganat_ja_katakanat")
-def page1():
-    return render_template("hiraganakatakana.html")
-
-@app.route("/sanastoa")
-def page2():
-    return render_template("words.html")
-
-@app.route("/kirjaudu_sisään")
-def page3():
-    return render_template("login.html")
-
-@app.route("/kirjaudu_sisään/luo_tunnus")
-def page4():
-    return render_template("register.html")
-
-@app.route("/pakka")
-def page5():
-    return render_template("deck.html")
+import routes
---
+++ routes.py
@@ -0,0 +1,29 @@
+from app import app
+from flask import render_template, request, redirect
+import decks
+import stats
+import users
+
+@app.route("/")
+def index():
+    return render_template("front_page.html")
+
+@app.route("/hiraganat_ja_katakanat")
+def page1():
+    return render_template("hiraganakatakana.html")
+
+@app.route("/sanastoa")
+def page2():
+    return render_template("words.html")
+
+@app.route("/kirjaudu_sisään")
+def page3():
+    return render_template("login.html")
+
+@app.route("/kirjaudu_sisään/luo_tunnus")
+def page4():
+    return render_template("register.html")
+
+@app.route("/pakka")
+def page5():
+    return render_template("deck.html")
