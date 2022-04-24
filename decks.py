49a16a4a5e31145f48c514fcd996810ef9524e07
Laura Peltonen <laura.peltonen53@gmail.com>
2022-04-24 21:58:51 +0300

    -

--- decks.py
+++ decks.py
@@ -8,8 +8,9 @@ def get_deck_size(deck_id):
     return db.session.execute("SELECT COUNT(*) FROM cards WHERE deck_id = :deck_id",{"deck_id":deck_id}).fetchone()[0]
 
 def get_reviews(deck_id):
-    return db.session.execute("""SELECT u.username, r.grade, r.comment FROM reviews r, users u WHERE r.user_id=u.id AND r.deck_id=:deck_id ORDER BY r.id""",{"deck_id": deck_id}).fetchall()
+    return db.session.execute("""SELECT u.username, r.grade, r.comment FROM reviews r, users u WHERE r.uder_id=u.id AND r.deck_id=:deck_id ORDER BY r.id""",{"deck_id": deck_id}).fetchall()
 #korjaa uder user:iksi
+
 def get_all_decks():
     return db.session.execute("SELECT id, deck_name FROM decks WHERE visible=1").fetchall()
 
@@ -30,12 +31,13 @@ def remove_deck(deck_id, user_id):
     db.session.execute("UPDATE decks SET visible=0 WHERE id:=id AND teacher_id=:user_id",{"id":deck_id, "user_id":user_id})
     db.session.commit()
 
+def add_review(deck_id, user_id, grades, comment):
+    db.session.execute("""INSERT INTO reviews(deck_id, user_id, grades, comment)  VALUES (:deck_id, :user_id, :stars, :comment)""", {"deck_id":deck_id, "user_id":user_id,"grades":grades, "comment":comment})
+    db.session.commit()
+
 #def get_random_card(deck_id):
 
 #def get_card_words(card_id):
 
 #def send_answer(card_id, answer, user_id):
 
-def add_review(deck_id, user_id, grades, comment):
-    db.session.execute("""INSERT INTO reviews(deck_id, user_id, grades, comment)  VALUES (:deck_id, :user_id, :stars, :comment)""", {"deck_id":deck_id, "user_id":user_id,"grades":grades, "comment":comment})
-    db.session.commit()
